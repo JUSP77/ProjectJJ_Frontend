db11b85024d111f97303c0a5d1b6920b3413fccf
jusp77 <jusp77@hanmail.net>
2023-07-13 21:47:02 +0900

    배포를 위해 port번호 수정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const { defineConfig } = require('@vue/cli-service');
 
-})
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    before: function (app) {
+      app.use(
+          '/api',
+          createProxyMiddleware({
+            target: 'http://localhost:8080',
+            changeOrigin: true,
+          })
+      );
+    },
+  },
+});
